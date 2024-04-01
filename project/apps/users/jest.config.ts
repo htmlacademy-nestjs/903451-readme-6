4198a5be942e2c2405ed77ed6cb56ae99f755042
Mykyta Slyvkanych <mykyta.slyvkanych@playtech.com>
2024-04-02 00:00:41 +0300

    module2-task1: Replace profile by users service

--- project/apps/users/jest.config.ts
+++ project/apps/users/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'profile',
+  displayName: 'users',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/profile'
+  coverageDirectory: '../../coverage/apps/users',
 };
