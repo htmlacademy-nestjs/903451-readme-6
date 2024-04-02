7b96fa266191e5e97ed26eaeb69005d1abba424b
Mykyta Slyvkanych <mykyta.slyvkanych@playtech.com>
2024-04-02 17:47:43 +0300

    module2-task1: Add authentication and blog user libs

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -1,4 +1,6 @@
 export { Entity } from './lib/base/entity';
+export { StorableEntity } from './lib/interfaces/storable-entity.interface';
+export { EntityFactory } from './lib/interfaces/entity-factory.interface';
 export { UserRole } from './lib/types/user-role.enum';
 export { User } from './lib/types/user.interface';
 export { AuthUser } from './lib/types/auth-user.interface';
