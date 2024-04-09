c25b8fbed3ffad79dc5cd69db291fa11e2ac4c45
Mykyta Slyvkanych <mykyta.slyvkanych@playtech.com>
2024-04-09 18:57:46 +0300

    module2-task1: bootstrap @nest/swagger and add spec for users service

--- project/libs/users/authentication/src/dto/change-user-password.dto.ts
+++ project/libs/users/authentication/src/dto/change-user-password.dto.ts
@@ -1,4 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class ChangeUserPasswordDto {
+  @ApiProperty({
+    description: 'Current user password',
+    example: '123456',
+  })
   public currentPassword: string;
+
+  @ApiProperty({
+    description: 'New user password',
+    example: '123456',
+  })
   public newPassword: string;
 }
--- project/libs/users/authentication/src/rdo/logged-user.rdo.ts
+++ project/libs/users/authentication/src/rdo/logged-user.rdo.ts
@@ -1,12 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 
 export class LoggedUserRdo {
+  @ApiProperty({
+    description: 'The uniq user ID',
+    example: '134ce8babd-cc30-4805-9b12-d9420398e7c5',
+  })
   @Expose()
   public id: string;
 
+  @ApiProperty({
+    description: 'User email',
+    example: 'example@gmail.com',
+  })
   @Expose()
   public email: string;
 
+  @ApiProperty({
+    description: 'User last name',
+    example: 'Jonson',
+  })
   @Expose()
   public accessToken: string;
 }
